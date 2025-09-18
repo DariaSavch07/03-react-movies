e5ef3257cbebc44d2cf2ed56eb4244d328635a00
Daria Savchenko <myworkpl24@gmail.com>
2025-09-18 18:28:48 +0200

    add

--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -1,20 +1,20 @@
-import styles from './MovieGrid.module.css';
-import type { Movie } from "../../types/movie";
-import css from "./MovieGrid.module.css";
+import React from 'react';
+import type { Movie } from '../../types/movie';
+import css from './MovieGrid.module.css';
 
 interface MovieGridProps {
   movies: Movie[];
   onSelect: (movie: Movie) => void;
 }
 
-export default function MovieGrid({ movies, onSelect }: MovieGridProps) {
-  if (movies.length === 0) return null;
+const MovieGrid: React.FC<MovieGridProps> = ({ movies, onSelect }) => {
+  if (!movies.length) return null;
 
   return (
     <ul className={css.grid}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <div className={css.card} onClick={() => onSelect(movie)}>
+        <li key={movie.id} onClick={() => onSelect(movie)}>
+          <div className={css.card}>
             <img
               className={css.image}
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
@@ -27,4 +27,6 @@ export default function MovieGrid({ movies, onSelect }: MovieGridProps) {
       ))}
     </ul>
   );
-}
+};
+
+export default MovieGrid;
